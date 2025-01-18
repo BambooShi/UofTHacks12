aa368c25b807452fd548f0e1338604f0e8698b1d
ansonkwokys <ansonyskwok@gmail.com>
2025-01-18 10:03:17 -0500

    fix: add Login type checking

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,5 +1,16 @@
-export default function Login() {
-    return(
-        null
-    )
-}
+import React from "react";
+
+const Login: React.FC = () => {
+    return (
+        <div className="Login-container">
+            <form>
+                <label htmlFor="player1">Player 1:</label>
+                <input type="text" id="player1" name="player1"></input>
+                <label htmlFor="player2">Player 2:</label>
+                <input type="text" id="player2" name="player2"></input>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
