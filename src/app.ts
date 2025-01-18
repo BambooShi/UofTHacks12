f4acdbe999459a09c59575ef928e84b03db42634
ansonkwokys <ansonyskwok@gmail.com>
2025-01-18 05:23:05 -0500

    feat: use typescript for app.js

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
+let express = require("express");
+import { NextFunction, Request, Response } from "express";
+
 require("dotenv").config();
 express = require("express");
 const PORT = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.send("Hello world");
 });
 
-app.use((err, req, res, next) => {
-    console.error(err);\
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
     res.status(err.statusCode || 500).send(err.message);
 });
 
