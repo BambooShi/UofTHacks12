12c1b09222320a6e45d48088aa89056ee9477135
ansonkwokys <ansonyskwok@gmail.com>
2025-01-18 21:49:58 -0500

    feat: add partial style for Game component

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -14,8 +14,8 @@ export default function Game() {
         const player1Name: string = location.state.player1Name || "player1";
         const player2Name: string = location.state.player2Name || "player2";
         return (
-            <div className="game-container">
-                <div className="game-play-container"></div>
+            <div className="flex flex-col justify-center items-center w-full h-full bg-gray-500">
+                <div className="bg-blue-700 w-10/12 min-h[220px]"></div>
                 <div className="camera-container"></div>
                 <div className="name-container">
                     <div>{player1Name}</div>
