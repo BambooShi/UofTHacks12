0918253e4491edebe9285f23224511f7544b8887
m2zhang <marilyn2zhang@gmail.com>
2025-01-18 22:50:58 -0500

    updates to jump/squat conditions

--- frontend/src/js/mediapipe.mjs
+++ frontend/src/js/mediapipe.mjs
@@ -34,6 +34,11 @@ const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
 
 // pt. 2 out
 
+const currentTime = Date.now();
+let lastJumpTime1 = 0;
+let lastJumpTime2 = 0;
+const jumpCooldown = 500;
+
 async function predictWebcam(video, canvasElement, callback) {
     //console.log("We predicting..");
     const canvasCtx = canvasElement.getContext("2d");
@@ -48,9 +53,12 @@ async function predictWebcam(video, canvasElement, callback) {
         runningMode = "VIDEO";
         await poseLandmarker.setOptions({ runningMode: "VIDEO" });
     }
+
     let startTimeMs = performance.now();
+
     if (lastVideoTime !== video.currentTime) {
         lastVideoTime = video.currentTime;
+
         poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
             canvasCtx.save();
             canvasCtx.clearRect(
@@ -66,9 +74,18 @@ async function predictWebcam(video, canvasElement, callback) {
             // before sending over move, check if the person's one part is less of the x component or wtv 
             // to see if they're on left side or right side.. then send over correspondingly that they're the 1st person..
 
-            // a person
+            // Person 1 fs
             const landmarks1 = result.landmarks[0];
+
             if (landmarks1){
+                let person;
+                if (landmarks1[0].x < 0.5){
+                    person = 1;
+                }
+                else{
+                    person = 2;
+                }
+                //console.log(`You're person #: ${person}`);
                 const leftHipY1 = landmarks1[23].y;
                 const rightHipY1 = landmarks1[24].y;
                 const leftKneeY1 = landmarks1[25].y;
@@ -77,36 +94,46 @@ async function predictWebcam(video, canvasElement, callback) {
                 //console.log("leftHandY is: ", leftHandY1);
                 const rightHandY1 = landmarks1[22].y;
         
-                const isJumping1 = leftHipY1 < 0.4 && rightHipY1 < 0.4;
+                const isJumping1 = leftHipY1 < 0.4 && rightHipY1 < 0.4 && leftHipY1 > 0.0 && rightHipY1 > 0.0 ;
                 const isSquatting1 = leftKneeY1 - leftHipY1 < 0.1 && rightKneeY1 - rightHipY1 < 0.1;
                 const isLeftHandUp1 = leftHandY1 < 0.5;
                 const isRightHandUp1 = rightHandY1 < 0.5;
         
-                if (isJumping1){
-                    // triggerAnimation("jump");
-                    console.log("person 1 jumping");
-                    //sendMotionData({ player2: "", player1: "jump" }); //temporary hardcoded
-                    
+                const currentTime = Date.now();
+                if (isJumping1 && (currentTime - lastJumpTime1 > jumpCooldown)) {
+                    console.log(`person ${person} jumping`);
+                    //sendMotionData({ player1: "jump" });
+
+                    lastJumpTime1 = currentTime; 
                 } else if (isSquatting1){
                     // triggerAnimation("squat");
-                    console.log("person 1 squatting");
+                    console.log(`person ${person} squatting`);
                     //sendMotionData({ player2: "", player1: "squat" }); //temporary hardcoded
                 
                 } 
-                 if (isLeftHandUp1){
+                 if (isLeftHandUp1 && (currentTime - lastJumpTime1 > jumpCooldown)){
                     // triggerAnimation("jump");
-                    console.log("person 1 squat");
+                    console.log(`person ${person} squat`);
+                    lastJumpTime1 = currentTime; 
                     //sendMotionData({ player2: "", player1: "squat" }); //temporary hardcoded
-                }  if (isRightHandUp1){
-                    console.log("person 1 jump");
+                }  if (isRightHandUp1 && (currentTime - lastJumpTime1 > jumpCooldown)){
+                    console.log(`person ${person} jump`);
+                    lastJumpTime1 = currentTime; 
                     //sendMotionData({ player2: "", player1: "jump" }); //temporary hardcoded
                 }
-                // Only overwrite missing pixels.
             }
 
             // another person
             const landmarks2 = result.landmarks[1];
             if (landmarks2){
+                let person;
+                if (landmarks2[0].x < 0.5){
+                    person = 1;
+                }
+                else{
+                    person = 2;
+                }
+
                 const leftHipY2 = landmarks2[23].y;
                 const rightHipY2 = landmarks2[24].y;
                 const leftKneeY2 = landmarks2[25].y;
@@ -115,31 +142,33 @@ async function predictWebcam(video, canvasElement, callback) {
                 //console.log("leftHandY of person 2 is: ", leftHandY2);
                 const rightHandY2 = landmarks2[22].y;
         
-                const isJumping2 = leftHipY2 < 0.4 && rightHipY2 < 0.4;
+                const isJumping2 = leftHipY2 < 0.4 && rightHipY2 < 0.4 && leftHipY2 > 0.1 && rightHipY2 > 0.1;
                 const isSquatting2 = leftKneeY2 - leftHipY2 < 0.1 && rightKneeY2 - rightHipY2 < 0.1;
                 const isLeftHandUp2 = leftHandY2 < 0.5;
                 const isRightHandUp2 = rightHandY2 < 0.5;
         
-                if (isJumping2){
-                    // triggerAnimation("jump");
-                    console.log("person 2 jumping");
-                    //sendMotionData({ player2: "", player1: "jump" }); //temporary hardcoded
-                    
+                const currentTime = Date.now();
+                if (isJumping2 && (currentTime - lastJumpTime2 > jumpCooldown)) {
+                    console.log(`person ${person} jumping`);
+                    // Send data for person 2's jump
+                    //sendMotionData({ player2: "jump" });
+
+                    lastJumpTime2 = currentTime; // Update the last jump time for person 1
                 } else if (isSquatting2){
                     // triggerAnimation("squat");
-                    console.log("person 2 squatting");
+                    console.log(`person ${person} squatting`);
                     //sendMotionData({ player2: "", player1: "squat" }); //temporary hardcoded
-                
+                    lastJumpTime2 = currentTime;
                 } 
-                 if (isLeftHandUp2){
+                 if (isLeftHandUp2 && (currentTime - lastJumpTime1 > jumpCooldown)){
                     // triggerAnimation("jump");
-                    console.log("person 2 squat");
+                    console.log(`person ${person} squat`);
                     //sendMotionData({ player2: "", player1: "squat" }); //temporary hardcoded
-                }  if (isRightHandUp2){
-                    console.log("person 2 jump");
+                }  if (isRightHandUp2 && (currentTime - lastJumpTime1 > jumpCooldown)){
+                    console.log(`person ${person} jump`);
+                    lastJumpTime2 = currentTime;
                     //sendMotionData({ player2: "", player1: "jump" }); //temporary hardcoded
                 }
-                // Only overwrite missing pixels.
             }
 
             for (const landmark of result.landmarks) {
